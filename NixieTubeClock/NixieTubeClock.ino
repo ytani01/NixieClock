c406158cd9ed91f89cfc7ed937c76429c292ffc9
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-08-10 20:55:08 +0900

    Start: 0.8.7(current)

--- NixieTubeClock/NixieTubeClock.ino
+++ NixieTubeClock/NixieTubeClock.ino
@@ -12,7 +12,7 @@
 #include "ModeTest2.h"
 
 static const String MY_NAME = "Nixie Tube Clock";
-int                 initValVer[NIXIE_NUM_N] = {0,0, 0,8, 0,6};
+int                 initValVer[NIXIE_NUM_N] = {0,0, 0,8, 0,7};
 
 #define LOOP_DELAY_US   2   // micro sbeconds
 #define DEBOUNCE        300 // msec
